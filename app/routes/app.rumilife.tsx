1f632ee89f5e84f489ce5e70d3ed22597da96f95
yang <1243602575@qq.com>
2026-01-27 09:43:26 +0800

    add sales data to rumilife page

--- app/routes/app.rumilife.tsx
+++ app/routes/app.rumilife.tsx
@@ -1,13 +1,28 @@
+const totalSales = [
+  { month: "2025-01", volume: "20294", revenue: "142178.79" },
+  { month: "2025-02", volume: "132651", revenue: "972772.07" },
+  { month: "2025-03", volume: "152291", revenue: "1108794.45" },
+  { month: "2025-04", volume: "155191.99", revenue: "1101058.06" },
+  { month: "2025-05", volume: "164373", revenue: "1193978.44" },
+  { month: "2025-06", volume: "149499.99", revenue: "1130003.54" },
+  { month: "2025-07", volume: "175160.97", revenue: "1353061.44" },
+  { month: "2025-08", volume: "192361.97", revenue: "1416268.56" },
+  { month: "2025-09", volume: "163781", revenue: "1149657.08" },
+  { month: "2025-10", volume: "164827.5", revenue: "1126331.96" },
+  { month: "2025-11", volume: "187949.98", revenue: "1351501.1" },
+  { month: "2025-12", volume: "323935.97", revenue: "2497961.99" },
+  { month: "2026-01", volume: "122981.98", revenue: "943791.01" },
+];
 
 export default function RumilifePage() {
   return (
     <s-page heading="Rumilife">
-      <s-box>
+      <s-section>
         <s-grid gridTemplateColumns="repeat(4, 1fr)">
-          <s-date-field defaultView="2025-09" defaultValue="2025-09-01" />
-          <s-date-field defaultView="2025-09" defaultValue="2025-09-01" />
+          <s-date-field defaultView="2025-01" defaultValue="2025-01-26" />
+          <s-date-field defaultView="2026-01" defaultValue="2026-01-25" />
         </s-grid>
-      </s-box>
+      </s-section>
 
       {/* total layout */}
       <s-grid
@@ -15,109 +30,30 @@ export default function RumilifePage() {
         gap="small"
         justifyContent="center"
       >
-        <s-section heading="Total Money">17654258.63</s-section>
-        <s-section heading="total number">5692359</s-section>
-        <s-section heading="person Money">59.36</s-section>
-        <s-section heading="person number">12.56</s-section>
+        <s-section heading="Sales Volume">2105300.35</s-section>
+        <s-section heading="Sales Revenue">15487358.49</s-section>
       </s-grid>
 
       {/* table content */}
       <s-section heading="">
         <s-table>
           <s-table-header-row>
-            <s-table-header listSlot="primary">Puzzle</s-table-header>
-            <s-table-header format="numeric">Pieces</s-table-header>
-            <s-table-header>Created</s-table-header>
-            <s-table-header listSlot="secondary">Status</s-table-header>
+            <s-table-header listSlot="primary">Month</s-table-header>
+            <s-table-header format="numeric">Sales Volume</s-table-header>
+            <s-table-header>Sales Revuenue</s-table-header>
           </s-table-header-row>
           <s-table-body>
-            <s-table-row clickDelegate="mountain-view-checkbox">
-              <s-table-cell>
-                <s-stack direction="inline" gap="small" alignItems="center">
-                  <s-checkbox id="mountain-view-checkbox" />
-                  <s-clickable
-                    href=""
-                    accessibilityLabel="Mountain View puzzle thumbnail"
-                    border="base"
-                    borderRadius="base"
-                    overflow="hidden"
-                    inlineSize="40px"
-                    blockSize="40px"
-                  >
-                    <s-image
-                      objectFit="cover"
-                      src="https://picsum.photos/id/29/80/80"
-                    />
-                  </s-clickable>
-                  <s-link href="">Mountain View</s-link>
-                </s-stack>
-              </s-table-cell>
-              <s-table-cell>16</s-table-cell>
-              <s-table-cell>Today</s-table-cell>
-              <s-table-cell>
-                <s-badge color="base" tone="success">
-                  Active
-                </s-badge>
-              </s-table-cell>
-            </s-table-row>
-            <s-table-row clickDelegate="ocean-sunset-checkbox">
-              <s-table-cell>
-                <s-stack direction="inline" gap="small" alignItems="center">
-                  <s-checkbox id="ocean-sunset-checkbox" />
-                  <s-clickable
-                    href=""
-                    accessibilityLabel="Ocean Sunset puzzle thumbnail"
-                    border="base"
-                    borderRadius="base"
-                    overflow="hidden"
-                    inlineSize="40px"
-                    blockSize="40px"
-                  >
-                    <s-image
-                      objectFit="cover"
-                      src="https://picsum.photos/id/12/80/80"
-                    />
-                  </s-clickable>
-                  <s-link href="">Ocean Sunset</s-link>
-                </s-stack>
-              </s-table-cell>
-              <s-table-cell>9</s-table-cell>
-              <s-table-cell>Yesterday</s-table-cell>
-              <s-table-cell>
-                <s-badge color="base" tone="success">
-                  Active
-                </s-badge>
-              </s-table-cell>
-            </s-table-row>
-            <s-table-row clickDelegate="forest-animals-checkbox">
-              <s-table-cell>
-                <s-stack direction="inline" gap="small" alignItems="center">
-                  <s-checkbox id="forest-animals-checkbox" />
-                  <s-clickable
-                    href=""
-                    accessibilityLabel="Forest Animals puzzle thumbnail"
-                    border="base"
-                    borderRadius="base"
-                    overflow="hidden"
-                    inlineSize="40px"
-                    blockSize="40px"
-                  >
-                    <s-image
-                      objectFit="cover"
-                      src="https://picsum.photos/id/324/80/80"
-                    />
-                  </s-clickable>
-                  <s-link href="">Forest Animals</s-link>
-                </s-stack>
-              </s-table-cell>
-              <s-table-cell>25</s-table-cell>
-              <s-table-cell>Last week</s-table-cell>
-              <s-table-cell>
-                <s-badge color="base" tone="neutral">
-                  Draft
-                </s-badge>
-              </s-table-cell>
-            </s-table-row>
+            {/* table row */}
+            {totalSales.map((sale) => (
+              <s-table-row
+                key={sale.month}
+                clickDelegate="mountain-view-checkbox"
+              >
+                <s-table-cell>{sale.month}</s-table-cell>
+                <s-table-cell>{sale.volume}</s-table-cell>
+                <s-table-cell>{sale.revenue}</s-table-cell>
+              </s-table-row>
+            ))}
           </s-table-body>
         </s-table>
       </s-section>
