712da649e709a9b182d12e3db10a4001941e88b9
yang <1243602575@qq.com>
2026-01-27 10:16:20 +0800

    adjust rumilife data

--- app/routes/app.rumilife.tsx
+++ app/routes/app.rumilife.tsx
@@ -1,20 +1,26 @@
+import { useState } from "react";
+
 const totalSales = [
-  { month: "2025-01", volume: "20294", revenue: "142178.79" },
-  { month: "2025-02", volume: "132651", revenue: "972772.07" },
-  { month: "2025-03", volume: "152291", revenue: "1108794.45" },
-  { month: "2025-04", volume: "155191.99", revenue: "1101058.06" },
-  { month: "2025-05", volume: "164373", revenue: "1193978.44" },
-  { month: "2025-06", volume: "149499.99", revenue: "1130003.54" },
-  { month: "2025-07", volume: "175160.97", revenue: "1353061.44" },
-  { month: "2025-08", volume: "192361.97", revenue: "1416268.56" },
-  { month: "2025-09", volume: "163781", revenue: "1149657.08" },
-  { month: "2025-10", volume: "164827.5", revenue: "1126331.96" },
-  { month: "2025-11", volume: "187949.98", revenue: "1351501.1" },
-  { month: "2025-12", volume: "323935.97", revenue: "2497961.99" },
-  { month: "2026-01", volume: "122981.98", revenue: "943791.01" },
+    { month: "2026-01", volume: "122981", revenue: "943791.01" },
+    { month: "2025-12", volume: "323935", revenue: "2497961.99" },
+    { month: "2025-11", volume: "187949", revenue: "1351501.1" },
+    { month: "2025-10", volume: "164827", revenue: "1126331.96" },
+    { month: "2025-09", volume: "163781", revenue: "1149657.08" },
+    { month: "2025-08", volume: "192361", revenue: "1416268.56" },
+    { month: "2025-07", volume: "175160", revenue: "1353061.44" },
+    { month: "2025-06", volume: "149499", revenue: "1130003.54" },
+    { month: "2025-05", volume: "164373", revenue: "1193978.44" },
+    { month: "2025-04", volume: "155191", revenue: "1101058.06" },
+    { month: "2025-03", volume: "152291", revenue: "1108794.45" },
+    { month: "2025-02", volume: "132651", revenue: "1052772.07" },
+    { month: "2025-01", volume: "20294", revenue: "142178.79" },
 ];
 
 export default function RumilifePage() {
+
+  // const [totalSalesVolume, setTotalSalesVolume] = useState(0);
+  // setTotalSalesVolume(totalSales.reduce((acc, curr) => acc + parseInt(curr.volume), 0));
+
   return (
     <s-page heading="Rumilife">
       <s-section>
@@ -25,14 +31,20 @@ export default function RumilifePage() {
       </s-section>
 
       {/* total layout */}
-      <s-grid
-        gridTemplateColumns="repeat(4, 1fr)"
-        gap="small"
-        justifyContent="center"
-      >
-        <s-section heading="Sales Volume">2105300.35</s-section>
-        <s-section heading="Sales Revenue">15487358.49</s-section>
-      </s-grid>
+      <s-section>
+        <s-grid
+          gridTemplateColumns="repeat(4, 1fr)"
+          gap="small"
+          justifyContent="center"
+        >
+          {/* <s-box border="base" borderRadius="base" background="transparent" overflow="hidden"> */}
+            <s-section heading="Sales Volume">2105300</s-section>
+          {/* </s-box> */}
+          {/* <s-box border="base" borderRadius="base" background="transparent" overflow="hidden"> */}
+            <s-section heading="Sales Revenue">16187358.49</s-section>
+          {/* </s-box> */}
+        </s-grid>
+      </s-section>
 
       {/* table content */}
       <s-section heading="">
