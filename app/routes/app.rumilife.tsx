2a4e66923972f76e4abdc55f79d01163db39e6c4
yang <1243602575@qq.com>
2026-01-28 17:22:23 +0800

    add recharts && echarts to implements cahrts functionality

--- app/routes/app.rumilife.tsx
+++ app/routes/app.rumilife.tsx
@@ -1,28 +1,56 @@
-import { useState } from "react";
+import { Line, LineChart, XAxis, YAxis } from "recharts";
+import ReactECharts from "echarts-for-react";
+// import { useEffect, useRef } from "react";
 
-const totalSales = [
-    { month: "2025-12", volume: "323935", revenue: "2747961.99" },
-    { month: "2025-11", volume: "187949", revenue: "1601501.1" },
-    { month: "2025-10", volume: "164827", revenue: "1376331.96" },
-    { month: "2025-09", volume: "163781", revenue: "1399657.08" },
-    { month: "2025-08", volume: "192361", revenue: "1666268.56" },
-    { month: "2025-07", volume: "175160", revenue: "1603061.44" },
-    { month: "2025-06", volume: "149499", revenue: "1380003.54" },
-    { month: "2025-05", volume: "164373", revenue: "1443978.44" },
-    { month: "2025-04", volume: "155191", revenue: "1351058.06" },
-    { month: "2025-03", volume: "152291", revenue: "1358794.45" },
-    { month: "2025-02", volume: "132651", revenue: "1402772.07" },
-    { month: "2025-01", volume: "143275", revenue: "1435969.8" },
+type ToTalSale = {
+  month: string;
+  volume: string;
+  revenue: string;
+  order: string;
+};
+
+const totalSales: ToTalSale[] = [
+  {
+    month: "2025-12",
+    volume: "323935",
+    revenue: "2747961.99",
+    order: "100827",
+  },
+  { month: "2025-11", volume: "187949", revenue: "1601501.1", order: "62690" },
+  { month: "2025-10", volume: "164827", revenue: "1376331.96", order: "54112" },
+  { month: "2025-09", volume: "163781", revenue: "1399657.08", order: "54666" },
+  { month: "2025-08", volume: "192361", revenue: "1666268.56", order: "67025" },
+  { month: "2025-07", volume: "175160", revenue: "1603061.44", order: "64248" },
+  { month: "2025-06", volume: "149499", revenue: "1380003.54", order: "55508" },
+  { month: "2025-05", volume: "164373", revenue: "1443978.44", order: "59485" },
+  { month: "2025-04", volume: "155191", revenue: "1351058.06", order: "54536" },
+  { month: "2025-03", volume: "152291", revenue: "1358794.45", order: "53636" },
+  { month: "2025-02", volume: "132651", revenue: "1402772.07", order: "47356" },
+  { month: "2025-01", volume: "143275", revenue: "1435969.8", order: "51500" },
 ];
 
 export default function RumilifePage() {
-
-  function sumVolume() {
-    return totalSales.reduce((acc, cur) => acc + parseInt(cur.volume), 0).toFixed(2);
+  function sumTotal(key: keyof ToTalSale) {
+    return totalSales
+      .reduce((acc, cur) => {
+        const value = cur[key];
+        return acc + (value ? parseFloat(value) : 0);
+      }, 0)
+      .toFixed(2);
   }
-  function sumRevenue() {
-    return totalSales.reduce((acc, cur) => acc + parseFloat(cur.revenue), 0).toFixed(2);
-  }
+  // const chartRef = useRef(null)
+  const option = {
+    title: { text: "销量统计" },
+    xAxis: { type: "category", data: ["衬衫", "羊毛衫", "雪纺衫"] },
+    yAxis: {},
+    series: [{ data: [5, 20, 36], type: "bar" }],
+  };
+  // useEffect(() => {
+  //   window.addEventListener("resize", () => { 
+  //     console.log("123")
+  //     console.log(chartRef.current)
+  //   });
+  // }, [])
 
   return (
     <s-page heading="Rumilife">
@@ -40,19 +68,50 @@ export default function RumilifePage() {
           gap="small"
           justifyContent="center"
         >
-            <s-section heading="Sales Volume">{sumVolume()}</s-section>
-            <s-section heading="Sales Revenue">{sumRevenue()}</s-section>
+          <s-section heading="Sales Volume">{sumTotal("volume")}</s-section>
+          <s-section heading="Sales Revenue">{sumTotal("revenue")}</s-section>
+          <s-section heading="Order Volume">{sumTotal("order")}</s-section>
         </s-grid>
       </s-section>
 
       {/* charts */}
-      {/* <s-grid gridTemplateColumns="repeat(2, 1fr)" gap="small" justifyContent="center">
-        <s-section heading="Sales Volume">
-        </s-section>
-        <s-section>
-
-        </s-section>
-      </s-grid> */}
+      <s-grid
+        gridTemplateColumns="repeat(4, 1fr)"
+        gap="small"
+        justifyContent="center"
+        padding="none none small none"
+      >
+        <s-grid-item gridColumn="span 2">
+          <s-section heading="Sales Volume">
+            <LineChart
+              style={{
+                width: "100%",
+                aspectRatio: 1.618,
+                margin: "auto",
+              }}
+              responsive
+              data={totalSales}
+            >
+              <XAxis
+                dataKey="month"
+                style={{ fontSize: "0.6rem" }}
+                label={{ offset: -10 }}
+              />
+              <YAxis
+                width="auto"
+                style={{ fontSize: "0.6rem" }}
+                label={{ offset: -1000 }}
+              />
+              <Line type="monotone" dataKey="volume" stroke="#13ACF0" />
+            </LineChart>
+          </s-section>
+        </s-grid-item>
+        {/* <s-grid-item> */}
+          <s-section>
+            <ReactECharts option={option} style={{ height: '100%', width: "100%" }} />
+          </s-section>
+        {/* </s-grid-item> */}
+      </s-grid>
 
       {/* table content */}
       <s-section heading="">
@@ -61,6 +120,7 @@ export default function RumilifePage() {
             <s-table-header listSlot="primary">Month</s-table-header>
             <s-table-header format="numeric">Sales Volume</s-table-header>
             <s-table-header>Sales Revuenue</s-table-header>
+            <s-table-header>Order Number</s-table-header>
           </s-table-header-row>
           <s-table-body>
             {/* table row */}
@@ -72,6 +132,7 @@ export default function RumilifePage() {
                 <s-table-cell>{sale.month}</s-table-cell>
                 <s-table-cell>{sale.volume}</s-table-cell>
                 <s-table-cell>{sale.revenue}</s-table-cell>
+                <s-table-cell>{sale.order}</s-table-cell>
               </s-table-row>
             ))}
           </s-table-body>
